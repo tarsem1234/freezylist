e3c39e3431bcdd6244bd6d0dafdc52799cc8325f
Shift <shift@laravelshift.com>
2025-01-07 09:33:57 +0000

    Convert controller validation to Form Requests

--- app/Http/Requests/Frontend/ConversationMessageRequest.php
+++ app/Http/Requests/Frontend/ConversationMessageRequest.php
@@ -8,11 +8,11 @@ class ConversationMessageRequest extends FormRequest
 {
     /**
      * Get the validation rules that apply to the request.
+     *
+     * @return array
      */
-    public function rules(): array
+    public function rules()
     {
-        return ['message' => [
-            'required',
-        ], ];
+        return ['message' => 'required'];
     }
 }
